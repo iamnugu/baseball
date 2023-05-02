57c07b12086e500a89228a47fe7c22a641be88b6
iamnugu <iamnugu22@gmail.com>
2023-05-02 17:12:58 +0900

    나머지 완료

--- base.py
+++ base.py
@@ -3,3 +3,17 @@ num = random.sample(range(10), 4)
 baseNum = ''.join(map(str, num))
 checkDict = dict()
 count = 10
+while count:
+    checkDict['S'] = 0
+    checkDict['B'] = 0
+    a = str(input())
+    for i in range(len(a)):                            #검증
+        if a[i] == baseNum[i]:
+            checkDict['S'] += 1
+        elif a[i] in baseNum:
+            checkDict['B'] += 1
+    print(f"{checkDict['S']}S, {checkDict['B']}B")
+    if checkDict['S'] == 4:
+        break
+    count -= 1
+print(f'축하합니다 {10 - count + 1}번의 도전으로 {baseNum}을 맞추셨네요 추카포카룽')
